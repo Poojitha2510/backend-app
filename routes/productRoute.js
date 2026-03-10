193c33831cec757dc3fef7274914bbb3e13a5429
Poojitha2510 <banothpoojitha435@gmail.com>
2026-03-10 13:42:38 +0530

    message

---
+++ routes/productRoute.js
@@ -0,0 +1,16 @@
+import { getProducts,addProductForm,addProduct,deleteProduct,editProductForm,saveProduct} from "../controllers/productController.js";
+import express from "express";
+
+const productRouter=express.Router()
+
+productRouter.get("/",getProducts);
+productRouter.get("/add",addProductForm)
+productRouter.post("/add",addProduct)
+productRouter.get("/:id/delete",deleteProduct)
+productRouter.get("/:id/edit",editProductForm)
+productRouter.post("/:id/edit",editProductForm)
+productRouter.post("/:id/save",saveProduct)
+
+
+
+export  {productRouter}
