9b33d0553a85072b2f90399a5db39717d3348648
Poojitha2510 <banothpoojitha435@gmail.com>
2026-03-11 22:56:23 +0530

    updated

--- models/productModel.js
+++ models/productModel.js
@@ -2,15 +2,9 @@
 import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
-   desc: { type: String, required: true },
+  desc: { type: String, required: true },
   price: { type: Number, required: true },
   imageUrl: { type: String },
 });
-const  productModel = mongoose.model("products",productSchema)
-
+const productModel = mongoose.model("products", productSchema);
 export default productModel;
-// const productModel=[
-//          {id:1,name:"Product1",price:100},
-//          {id:2,name:"Product2",price:150},
-//          {id:3,name:"Product3",price:120}
-// ];
